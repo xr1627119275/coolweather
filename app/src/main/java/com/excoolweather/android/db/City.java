0a141127753ad1d17e580e53dc3397f61b8de0f4
xr <1627119275@qq.com>
2017-02-11 14:55:58 +0800

    加入数据库和表的配置.

---
+++ app/src/main/java/com/excoolweather/android/db/City.java
@@ -0,0 +1,30 @@
+package com.excoolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/2/11.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+}
